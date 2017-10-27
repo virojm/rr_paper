e2342e72c979ba6c2a0b12d0a4940ac8b6f5230d
virojm <viroj.mettanant@gmail.com>
2017-10-27 16:30:00 +0700

    [ENH] report example

---
+++ rr_report/__openerp__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'JRXML Reports',
+    'version': '0.1',
+    'category': 'Tools',
+    'author': 'Viroj, Technisoft Co., Ltd',
+    'depends': ['purchase'],
+    'data': [
+        'wizard/wip_report_wizard.xml',
+        'data/report_data.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
