15c0c903593901693d98edd2bccde68bf31e1e2d
virojm <viroj.mettanant@gmail.com>
2017-12-04 20:10:24 +0700

    Add sub-assembly items to BOM line

--- mrp_bom/models/bomline.py
+++ mrp_bom/models/bomline.py
@@ -4,4 +4,4 @@ class MRPBomLine(models.Model):
     _inherit = 'mrp.bom.line'
 
     product_id = fields.Many2one(
-        'product.product', 'Product', domain=[('purchase_ok','=', True)], required=True)
+        'product.product', 'Product', domain=[('sale_ok','!=', True)], required=True)
