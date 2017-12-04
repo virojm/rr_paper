48880aa77c0bec72e30dec526877e81403fd37db
virojm <viroj.mettanant@gmail.com>
2017-12-04 19:03:27 +0700

    Add filter to BOM-see only RM

---
+++ mrp_bom/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'RR Paper Products: MRP',
+    'version': '0.1',
+    'category': 'Manufacturing',
+    'author': 'Viroj, Technisoft Co., Ltd',
+    'depends': ['base', 'mrp'],
+    'data': [
+        'views/mrp_line_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+
+}
