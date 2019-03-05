7bdd40c5b43df8471e3a5072b3decb644450e21a
Tawanrut <tawanrut.kin@gmail.com>
2019-03-05 13:29:22 +0700

    [ADD][rr_form]: Add report Qweb for purchase

---
+++ rr_form/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Qweb Reports',
+    'version': '0.1',
+    'category': 'Tools',
+    'author': 'Viroj, Technisoft Co., Ltd',
+    'depends': [
+        'base',
+        'purchase',
+    ],
+    'data': [
+        'report/report_purchase_templates.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ rr_form/models/res_partner.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import base64
+import datetime
+import hashlib
+import pytz
+import threading
+import urllib2
+import urlparse
+
+from email.utils import formataddr
+from lxml import etree
+
+from odoo import api, fields, models, tools, SUPERUSER_ID, _
+from odoo.modules import get_module_resource
+from odoo.osv.expression import get_unaccent_wrapper
+from odoo.exceptions import UserError, ValidationError
+from odoo.osv.orm import browse_record
+
+ADDRESS_FORMAT_CLASSES = {
+    '%(city)s %(state_code)s\n%(zip)s': 'o_city_state',
+    '%(zip)s %(city)s': 'o_zip_city'
+}
+class FormatAddress(object):
+    @api.model
+    def fields_view_get_address(self, arch):
+        address_format = self.env.user.company_id.country_id.address_format or ''
+        for format_pattern, format_class in ADDRESS_FORMAT_CLASSES.iteritems():
+            if format_pattern in address_format:
+                doc = etree.fromstring(arch)
+                for address_node in doc.xpath("//div[@class='o_address_format']"):
+                    # add address format class to address block
+                    address_node.attrib['class'] += ' ' + format_class
+                    if format_class.startswith('o_zip'):
+                        zip_fields = address_node.xpath("//field[@name='zip']")
+                        city_fields = address_node.xpath("//field[@name='city']")
+                        if zip_fields and city_fields:
+                            # move zip field before city field
+                            city_fields[0].addprevious(zip_fields[0])
+                arch = etree.tostring(doc)
+                break
+        return arch
+
+
+
+class Partner(models.Model, FormatAddress):
+    _description = 'Partner'
+    _inherit = "res.partner"
+
+    @api.multi
+    def name_get(self):
+        res = []
+        for partner in self:
+            name = partner.name or ''
+
+            if partner.company_name or partner.parent_id:
+                if not name and partner.type in ['invoice', 'delivery', 'other']:
+                    name = dict(self.fields_get(['type'])['type']['selection'])[partner.type]
+                if not partner.is_company:
+                    name = "%s, %s" % (partner.commercial_company_name or partner.parent_id.name, name)
+            if self._context.get('show_address_only'):
+                name = partner._display_address(without_company=True)
+            if self._context.get('show_address'):
+                name = name + "\n" + partner._display_address(without_company=True)
+            name = name.replace('\n\n', '\n')
+            name = name.replace('\n\n', '\n')
+            if self._context.get('show_email') and partner.email:
+                name = "%s <%s>" % (name, partner.email)
+            if self._context.get('html_format'):
+                name = name.replace('\n', '<br/>')
+            res.append((partner.id, name))
+        return res
+
+    @api.multi
+    def _display_address(self, without_company=False):
+
+        '''
+        The purpose of this function is to build and return an address formatted accordingly to the
+        standards of the country where it belongs.
+
+        :param address: browse record of the res.partner to format
+        :returns: the address formatted in a display that fit its country habits (or the default ones
+            if not country is specified)
+        :rtype: string
+        '''
+        # get the information that will be injected into the display format
+        # get the address format
+        address_format = self.country_id.address_format or \
+              "%(street)s\n%(street2)s\n%(city)s %(state_code)s %(zip)s\n%(country_name)s"
+        args = {
+            'state_name': self.state_id.name or '',
+            'state_code': self.state_id.name or '',
+            'country_code': self.country_id.code or '',
+            'country_name': self.country_id.name or '',
+            'company_name': self.commercial_company_name or '',
+        }
+        for field in self._address_fields():
+            args[field] = getattr(self, field) or ''
+        if without_company:
+            args['company_name'] = ''
+        elif self.commercial_company_name:
+            address_format = '%(company_name)s\n' + address_format
+        return address_format % args
+
+    def _display_address_depends(self):
+        # field dependencies of method _display_address()
+        return self._address_fields() + [
+            'country_id.address_format', 'country_id.code', 'country_id.name',
+            'company_name', 'state_id.name', 'state_id.name',
+        ]
